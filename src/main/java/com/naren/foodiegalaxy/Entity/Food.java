04e4d3a222b10dd1148eac1c380aa47f6450a886
Naren <Naren0625199@gmail.com>
2023-01-02 13:01:58 +0530

    FirstCommit

--- src/main/java/com/naren/foodiegalaxy/Entity/Food.java
+++ src/main/java/com/naren/foodiegalaxy/Entity/Food.java
@@ -1,5 +1,26 @@
 package com.naren.foodiegalaxy.Entity;
 
-public class Food {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "numberServed", nullable = false)
+    private Integer numberServed;
 }
